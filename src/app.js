5afb7fa3e8fa4145a16eb61ce7ab5a98129cfbd6
aliAkkayaJ <aliakkayaa0gmail.com>
2023-01-11 18:33:51 +0300

    18 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,31 @@
 console.log("Merhaba Kodlama.io")
+//JS type safe değildir. Tanımladığın tür dışında atama yapabilirsin
 
-//JS type safe değildir
-let dolarBugun = 9.30
+let dolarBugun = 9.30  //değişkenler var yerine let ile tanımlanır.
+// var global değişken gibi çalışır.İşimize gelmez.
+let dolarDun = 9.20 
 
-let dolarDun = 9.20
-dolarDun = "9.20"
 {
-    let dolarDun = 9.10
-}
+    let dolarDun = 9.10 
 
+}
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
+const euroDun = 11.2 
+// euroDun=11 sabit olduğu için değiştiremezsin.
 
 console.log(euroDun)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+//arrayler
+//isimlendirmede camelCase kullanılır.
+//PascalCasing ilk harfler büyük.
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+//ileride backenden alıcaz -> let konutKredileri =getFromApi()
+console.log("<ul>") //üstteki dğerleri dönmem lazım ne kadar geleceğini bilimiyorum.
+for(let i=0;i<konutKredileri.length;i++){
     console.log("<li>"+konutKredileri[i]+"</li>")
 }
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
 console.log(konutKredileri)
