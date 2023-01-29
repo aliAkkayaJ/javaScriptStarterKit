c577943f8f7483c98b5f5844f2f8a31309a0a003
aliAkkayaJ <aliakkayaa0gmail.com>
2023-01-29 16:09:39 +0300

    22 kasım ders başı

--- src/crossCuttingConcerns/logging/logger.js
+++ src/crossCuttingConcerns/logging/logger.js
@@ -1,18 +1,17 @@
-export class BaseLogger{
-    log(data){
-        console.log("Default logger : " + data)
+//loglama,doğrulama,catch //farklı yerlere logları yazmak istiyorsak aşağıdaki gibi
+export class BaseLogger{  //burası bizim için şablon
+    log(data){  
+        console.log("default loger"+data)
     }
 }
 
-export class ElasticLogger extends BaseLogger{
-    log(data){
-        console.log("Logged to Elastic" + data)
+export class ElasticLogger extends BaseLogger{ //inherite ettik.dedik sen bir baseLoggersın
+    log(data){  
+        console.log("Logged to Elastic" +data)
     }
 }
-
-export class MongoLogger extends BaseLogger{
-    log(data){
-        console.log("Logged to Mongo" + data)
+export class MongoLogger extends BaseLogger{ 
+    log(data){  
+        console.log("Logged to Mongo" +data)
     }
-}
-
+} //extends olması ne işe yaradı ? yani sen baseLoggerdan üretildin eğer içinde bir şey olmazsa base logger'ın datası sende olur. Ama eğer kendine has daya varsa base loggerın datasını ezersin. Yani baseLogger'ın içindeki her şeye extend ettiğimiz şeyde de eğer bir şey yoksa sahip oluyoruz.
--- src/models/customer.js
+++ src/models/customer.js
@@ -1,8 +1,6 @@
-import User from "./user.js"
-
-export default class Customer extends User{
-    constructor(id, firstName, lastName, city,age, creditCardNumber) {
-        super(id, firstName, lastName, city,age)
-        this.creditCardNumber = creditCardNumber
+export default class Customer extends User{ //userda olan ortak alanları burada yazmak zorunda kalmam artık extends ile
+constructor(id,firstName,lastName,city,age,creditCardNumber){        super(id,firstName,lastName,city,age) //yani burada userdaki yerleri çalıştırır.               
+        this.creditCardNumber=creditCardNumber
     }
+
 }
